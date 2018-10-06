4466581bd76a0f51d281e3c97bbf35e19c151a58
panasandu <panasandu@gmail.com>
2018-10-06 17:09:09 +0300

    first commit

---
+++ src/main/java/sok/spring/sokpetclinic/SokPetClinicApplication.java
@@ -0,0 +1,12 @@
+package sok.spring.sokpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokPetClinicApplication.class, args);
+	}
+}
