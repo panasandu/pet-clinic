b0fd20b679bbce110cef05f71d200e5fe4f8645d
panasandu <panasandu@gmail.com>
2018-10-10 20:08:33 +0300

    closes #5

---
+++ pet-clinic-data/src/main/java/sok/spring/sokpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package sok.spring.sokpetclinic.services;
+
+import sok.spring.sokpetclinic.model.Owner;
+import sok.spring.sokpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
